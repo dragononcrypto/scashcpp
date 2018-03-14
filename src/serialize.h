70d34640344c936cbc7fb2d6cb68c90f71ac6e1a
Scashdevs <admin@scash.ml>
2018-03-14 09:47:54 +0300

    linux specifics

--- src/serialize.h
+++ src/serialize.h
@@ -824,7 +824,20 @@ public:
             vch.insert(it, first, last);
     }
 
-
+#if !defined(_MSC_VER) && !defined(Q_OS_MAC)
+void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
+    {
+        assert(last - first >= 0);
+        if (it == vch.begin() + nReadPos && (unsigned int)(last - first) <= nReadPos)
+        {
+            // special case for inserting at the front when there's room
+            nReadPos -= (last - first);
+            memcpy(&vch[nReadPos], &first[0], last - first);
+        }
+        else
+            vch.insert(it, first, last);
+    }
+#endif
 
 #if !defined(_MSC_VER) || _MSC_VER >= 1300
     void insert(iterator it, const char* first, const char* last)
