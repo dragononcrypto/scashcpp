237dd97e4121614588398e208042af354d6906eb
ScashDevelopers <admin@scash.ml>
2018-03-14 09:07:15 +0300

    Fix libs update

--- buildsystem/mac/fix_libs_qt550.sh
+++ buildsystem/mac/fix_libs_qt550.sh
@@ -1,4 +1,8 @@
+## Exe dir:
+
 install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib @executable_path/../Frameworks/libssl.1.0.0.dylib ./scash-qt
+
+install_name_tool -change /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks/libcrypto.1.0.0.dylib ./scash-qt
 
 install_name_tool -change /usr/local/opt/berkeley-db@4/lib/libdb_cxx-4.8.dylib @executable_path/../Frameworks/libdb_cxx-4.8.dylib ./scash-qt
 
@@ -16,13 +20,7 @@ install_name_tool -change /Users/admin/Qt5.4.2/5.4/clang_64/lib/QtGui.framework/
 
 install_name_tool -change /Users/admin/Qt5.4.2/5.4/clang_64/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./scash-qt
 
+### In Frameworks:
 
-#	/usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib (compatibility version 1.0.0, current version 1.0.0)
-#	/usr/local/opt/berkeley-db@4/lib/libdb_cxx-4.8.dylib (compatibility version 0.0.0, current version 0.0.0)
-#	/usr/local/opt/boost@1.60/lib/libboost_system-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
-#	/usr/local/opt/boost@1.60/lib/libboost_filesystem-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
-#	/usr/local/opt/boost@1.60/lib/libboost_program_options-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
-#	/usr/local/opt/boost@1.60/lib/libboost_thread-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
-#	/Users/admin/Qt5.4.2/5.4/clang_64/lib/QtWidgets.framework/Versions/5/QtWidgets (compatibility version 5.4.0, current version 5.4.2)
-#	/Users/admin/Qt5.4.2/5.4/clang_64/lib/QtGui.framework/Versions/5/QtGui (compatibility version 5.4.0, current version 5.4.2)
-#	/Users/admin/Qt5.4.2/5.4/clang_64/lib/QtCore.framework/Versions/5/QtCore (compatibility version 5.4.0, current version 5.4.2)
+install_name_tool -change /usr/local/Cellar/openssl/1.0.2n/lib/libcrypto.1.0.0.dylib @executable_path/../Frameworks//libcrypto.1.0.0.dylib ./libssl.1.0.0.dylib
+
