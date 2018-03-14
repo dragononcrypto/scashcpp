5d95fc7c44a37413bdb11e4408541685521448dc
ScashDevelopers <admin@scash.ml>
2018-03-14 09:04:49 +0300

    Removed unused stuff

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/Scash.ico
-
-convert ../../src/qt/res/icons/Scash-16.png ../../src/qt/res/icons/Scash-32.png ../../src/qt/res/icons/Scash-48.png ${ICON_DST}
